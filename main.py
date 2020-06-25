5df4610fbd0ac8ef4368c14c1e168a1e8cf3dd7d
allisondoggett <ald912@lvie.missouristate.edu>
2020-06-25 16:58:08 -0500

    US1:TS06
    Background subtractor

--- main.py
+++ main.py
@@ -15,6 +15,29 @@ while(capture.isOpened()):
     capture.release() 
     cv2.destroyAllWindows() 
 else: 
-    print("Camera is not connected") 
+    print("Camera is not connected")
+
+
+
+import numpy as np 
+import cv2 
+  
+#parentheses in next line will contain video name when determined
+cap = cv2.VideoCapture() 
+  
+fgbg = cv2.createBackgroundSubtractorMOG2() 
+  
+while(1): 
+    ret, frame = cap.read() 
+    
+    fgmask = fgbg.apply(frame) 
+  
+    cv2.imshow('frame', fgmask)   
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: 
+        break
+  
+cap.release() 
+cv2.destroyAllWindows()
 
 
