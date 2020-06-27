a4d28be502cb1e8171167e4ae2bd3b5e0e25bec7
manikv12 <49544491+manikv12@users.noreply.github.com>
2020-06-26 20:23:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,32 +2,12 @@ import numpy as np
 import cv2 
   
   
-capture = cv2.VideoCapture(0) 
-while(capture.isOpened()): 
-      
-    while True: 
-          
-        ret, img = capture.read() 
-        cv2.imshow('img', img) 
-        if cv2.waitKey(30) & 0xff == ord('x'): 
-            break
-              
-    capture.release() 
-    cv2.destroyAllWindows() 
-else: 
-    print("Camera is not connected")
-
-
-
-import numpy as np 
-import cv2 
-  
 #parentheses in next line will contain video name when determined
 cap = cv2.VideoCapture() 
   
-fgbg = cv2.createBackgroundSubtractorMOG2() 
+fgbg = cv2.createBackgroundSubtractorKNN() 
   
-while(1): 
+while(True): 
     ret, frame = cap.read() 
     
     fgmask = fgbg.apply(frame) 
@@ -39,5 +19,3 @@ while(1):
   
 cap.release() 
 cv2.destroyAllWindows()
-
-
