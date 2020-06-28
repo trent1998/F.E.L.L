1a9746a6042c50c7667838a3f5b91642e91b5483
manikv12 <49544491+manikv12@users.noreply.github.com>
2020-06-27 20:06:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,65 @@
-import numpy as np 
-import cv2 
-  
-  
-capture = cv2.VideoCapture(0) 
-while(capture.isOpened()): 
-      
-    while True: 
-          
-        ret, img = capture.read() 
-        cv2.imshow('img', img) 
-        if cv2.waitKey(30) & 0xff == ord('x'): 
-            break
-              
-    capture.release() 
-    cv2.destroyAllWindows() 
-else: 
-    print("Camera is not connected") 
-    
-    
-    
-    
-# loop for accessing frames of the video
-while True:
-	frame = vs.read()
-	frame = frame if args.get("video", None) is None else frame[1]
-	if frame is None:
-		break
+import cv2
+import time
+
+capture = cv2.VideoCapture(0)
+time.sleep(2)
 
 
+# Detect the moving part in the image and remove the still background
+backSubtractor = cv2.createBackgroundSubtractorKNN(detectShadows=False)
+counter = 0
+
+while(True):
+    #Convert the video into the frames
+    ret, frame = capture.read()
+    
+    #Convert all the frame to gray scale and subtract the background
+    try:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert the image to grayscale
+        fgmask = backSubtractor.apply(gray)  #background subtraction
+        
+        #Find contours of the object that is stored as foregroundmask in variable fgmask
+        contours, _ = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if contours:
+        
+            # A list that will hold areas of all the moving objects in the frames
+            areas = []
+            #Get area of the object in the frame and then append it
+            for contour in contours:
+                area = cv2.contourArea(contour)
+                areas.append(area)
+            
+            #Find the object with the maximum area in the array 
+            maxArea = max(areas, default = 0)
+
+            maxAreaIdx = areas.index(maxArea) #index of object with maximum area
+
+            cnt = contours[maxAreaIdx]
+
+            M = cv2.moments(cnt)
+            
+            x, y, w, h = cv2.boundingRect(cnt)
+
+            cv2.drawContours(fgmask, [cnt], 0, (255,255,255), 3, maxLevel = 0)
+            
+            if h < w:
+                counter += 1
+                
+            if counter > 14:
+                cv2.putText(frame, 'FALL', (x, y), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255,255,255), 2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+                
+            #If the person sands up again or has not fallen counter will be 0
+            if h > w:
+                counter = 0 
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+            cv2.imshow('F.E.L.L', frame)
+        
+            if cv2.waitKey(33) == 27:
+             break
+    except:
+        break
+cv2.destroyAllWindows()
